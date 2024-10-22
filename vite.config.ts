c18ef80ab90c37450ed421f138e12ec762a5e279
johnlloydserrano <jlsrno@gmail.com>
2024-10-22 11:26:54 +0800

    JLS-9 Initial project setup

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@images': path.resolve(__dirname, './public/images'),
+    },
+  },
+})
