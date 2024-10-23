27d554fa1aad7e7a845a286c4b6ab485f4c68788
johnlloydserrano <jlsrno@gmail.com>
2024-10-23 08:21:52 +0800

    format source code

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,9 +9,7 @@ export default function Banner() {
                 <div className="py-10">
                   <div className="space-y-8 px-4">
                     <img className="h-24" src="/images/serrano.png" alt="johnlloydserrano" />
-                    <h1 className="text-5xl leading-tight roboto-regular tracking-wide py-2">
-                      Hello! I'm John Lloyd Serrano
-                    </h1>
+                    <h1 className="text-5xl leading-tight roboto-regular tracking-wide py-2">Hello! I'm John Lloyd Serrano</h1>
                   </div>
                 </div>
               </div>
@@ -20,13 +18,10 @@ export default function Banner() {
               <div className="flex items-center flex-wrap h-full px-4 py-10 space-y-6">
                 <h2 className="roboto-regular leading-tight text-5xl pr-12">A Web developer from Cebu City, Philippines.</h2>
                 <p className="roboto-regular text-lg text-slate-500">
-                  A passionate individual who finds deep fulfillment and joy in the journey of crafting meaningful
-                  experiences.
+                  A passionate individual who finds deep fulfillment and joy in the journey of crafting meaningful experiences.
                 </p>
                 <div className="space-x-6">
-                  <button
-                    className="roboto-medium text-lg bg-primary border border-primary tracking-wide text-white py-2 px-6 rounded-lg"
-                  >
+                  <button className="roboto-medium text-lg bg-primary border border-primary tracking-wide text-white py-2 px-6 rounded-lg">
                     Talk with me
                   </button>
                   <button className="roboto-light text-lg bg-white border rounded-lg py-2 px-6">Download CV</button>
@@ -37,5 +32,5 @@ export default function Banner() {
         </div>
       </div>
     </section>
-  )
+  );
 }
