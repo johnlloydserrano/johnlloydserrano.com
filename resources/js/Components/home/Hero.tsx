c0a86b4ae9d998d204a3621819beb1ef3e8039aa
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2025-03-20 13:58:38 +0800

    add privacy policy

--- resources/js/Components/home/Hero.tsx
+++ resources/js/Components/home/Hero.tsx
@@ -29,7 +29,10 @@ export default function HeroSection({ heroData, isLoading }: Props) {
                 </div>
                 <div className="hero-section-box w-full flex flex-col lg:flex-row items-center justify-center">
                     <div className="w-full lg:w-1/2 flex flex-col items-center text-center lg:text-left">
-                        <img className="mt-6 w-60 sm:w-72 md:w-80 lg:w-96" src={heroData.imageUrl} alt={heroData.name} />
+                        <div
+                            className="mt-6 w-60 sm:w-72 md:w-80 lg:w-96 h-60 sm:h-72 md:h-80 lg:h-96 bg-center bg-contain bg-no-repeat"
+                            style={{ backgroundImage: `url(${heroData.imageUrl})` }}
+                        />
                         <div className="mt-6 w-full flex flex-col sm:flex-row justify-center items-center gap-4">
                             <Button
                                 className="w-full sm:w-auto px-6 py-3 relative overflow-hidden group"
