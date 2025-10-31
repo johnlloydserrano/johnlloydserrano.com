8941ea3867c9e0bb2bc7bd3126e5e125dce5bcd0
jl-serrano <serranojohnlloyd@bpoc.co.jp>
2025-10-31 09:41:13 +0800

    feat: embed product hunt on footer  and adjust buttons

--- src/app/(pages)/components/Sections/Skill/index.tsx
+++ src/app/(pages)/components/Sections/Skill/index.tsx
@@ -1,10 +1,8 @@
 import { motion } from 'framer-motion';
-import { Download } from 'lucide-react';
 import SectionHeader from '../components/SectionHeader';
 import { SkillSkeleton } from '../../Skeletons';
 import { CardListGrids } from '../components/CardListGrid';
 import { Skill } from '@/models/skills/types';
-import { Button } from '@/app/components/atoms/Button';
 import { useTranslation } from 'react-i18next';
 
 interface SkillSectionProps {
@@ -16,11 +14,6 @@ export default function SkillSection({
   skillData,
   isLoading,
 }: SkillSectionProps) {
-  const handleDownload = (url: string) => {
-    const link = document.createElement('a');
-    link.href = url;
-    link.click();
-  };
   const { t } = useTranslation();
 
   return (
@@ -44,30 +37,6 @@ export default function SkillSection({
                 cardContentClass="p-4"
               />
             )}
-            <div className="flex justify-center items-center space-x-4 md:space-x-6">
-              {/* onClick={() => handleDownload(route('download.resume'))} */}
-              <Button
-                className="rounded-full px-6 py-3 w-auto relative overflow-hidden group"
-                variant="primary"
-              >
-                <span className="absolute left-0 top-0 h-full w-0 bg-linear-to-tr from-primary to-secondary transition-all duration-300 ease-out group-hover:w-full"></span>
-                <span className="relative z-10 flex items-center gap-2">
-                  <Download />
-                  {t('resume')}
-                </span>
-              </Button>
-              {/* onClick={() => handleDownload(route('download.skillsheet'))} */}
-              <Button
-                className="rounded-full px-6 py-3 w-auto relative overflow-hidden group"
-                variant="primary"
-              >
-                <span className="absolute left-0 top-0 h-full w-0 bg-linear-to-tr from-primary to-secondary transition-all duration-300 ease-out group-hover:w-full"></span>
-                <span className="relative z-10 flex items-center gap-2">
-                  <Download />
-                  {t('skillsheet')}
-                </span>
-              </Button>
-            </div>
           </div>
         </motion.div>
       </div>
